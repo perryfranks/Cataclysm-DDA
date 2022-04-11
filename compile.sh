c2e36a8385968e2735afddd4a65aeb7e1f5491a9
Perry Franks <z5165380@ad.unsw.edu.au>
2022-04-12 01:41:24 +1000

    started adding items

--- compile.sh
+++ compile.sh
@@ -1,23 +1,49 @@
-#!/bin/bash
+#!/bin/sh
 
 # a file to remember how we like to compile something
 # -t compiles with tiles
+# -s runs the linter first
+
+c=0; # c = have compiled game 
 
 #check if we need to fix style 
-i=1; 
 for cmd in "$@"
 do 
-    echo "hello"
-    if [[ $i == "-s" ]]; then 
-        echo "hello"
+    if [ $cmd = "-s" ] 
+    then 
+        echo "Running style-json"
         make style-json
     fi
-    i=$((i + 1));
 done
 
-# must have space before a backet apparently
-if [[ $1 == *"-t"* ]]; then 
-    make -j18 TILES=1 SOUND=0 RELEASE=1
-else 
-    make -j18 TILES=0 SOUND=0 RELEASE=1
-fi  
+# run make clean first
+for cmd in "$@"
+do 
+    if [ $cmd = "-c" ] 
+    then 
+        echo "Running clean"
+        make clean
+    fi
+done
+
+for cmd in "$@"
+do 
+    if [ $cmd = "-t" ] 
+    then 
+        echo "Tile make"
+        echo "make -j18 TILES=1 LOCALIZE=0 SOUND=0 RELEASE=1"
+        c=1;
+        make -j18 TILES=1 SOUND=0 LOCALIZE=0 RELEASE=1
+    fi
+done
+
+# if we have not compiled so far then we should with the default compile option
+if [ $c -eq 0 ] 
+then
+    echo "Default compile running"
+    echo "make -j18 TILES=0 SOUND=0 RELEASE=1"
+    make -j18 TILES=0 SOUND=0 LOCALIZE=0 RELEASE=1
+fi 
+
+
+echo "Compile complete"
